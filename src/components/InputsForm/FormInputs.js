2d8c356c864612edfb3fb31bc7f5619b4c2c0375
thibaud aptel <thibaudaptel@thibauds-MBP.modem>
2022-09-26 14:18:01 +1000

    form Validations

--- src/components/InputsForm/FormInputs.js
+++ src/components/InputsForm/FormInputs.js
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import Card from "../UI/Card";
 import styles from "./FormInputs.module.css";
 import Button from "../UI/Button";
+import ErrorModal from "../UI/ErrorModal";
 
 const FormInputs = (props) => {
   const [enteredFirstName, setEnteredFirstName] = useState("");
@@ -11,6 +12,7 @@ const FormInputs = (props) => {
     useState("");
   const [enteredStartDate, setEnteredStartDate] = useState("");
   const [enteredEndDate, setEnteredEndDate] = useState("");
+  const [error, setError] = useState();
 
   const lastNameChangeHandler = (event) => {
     setEnteredLastName(event.target.value);
@@ -33,6 +35,34 @@ const FormInputs = (props) => {
   };
   const submitHandler = (event) => {
     event.preventDefault();
+
+    // Form Validations
+    if (
+      enteredFirstName.trim().length === 0 ||
+      enteredLastName.trim().length === 0
+    ) {
+      setError({
+        title: "Invalid Lastname or Firstname",
+        message: "Please enter a valid name",
+      });
+      return;
+    }
+    if (enteredAnnualSalary.trim().length === 0) {
+      setError({
+        title: "Annual Salary Missing",
+        message: "Please enter a valid salary",
+      });
+      return;
+    }
+    if (enteredSuperannuationRate.trim().length === 0) {
+      setError({
+        title: "Superannuation Rate Missing",
+        message: "Please enter a valid Superannuation Rate",
+      });
+      return;
+    }
+
+    // props
     props.onSubmit();
     props.onAddPayslip(
       enteredFirstName,
@@ -52,64 +82,77 @@ const FormInputs = (props) => {
     return;
   };
 
+  const errorHandler = () => {
+    setError(null);
+  };
+
   return (
-    <Card className={styles.input}>
-      <form onSubmit={submitHandler}>
-        <div className="form-inputs-controls">
-          <div className="form-input-control">
-            <input
-              placeholder="Firstname"
-              type="text"
-              value={enteredFirstName}
-              onChange={firstNameChangeHandler}
-            />
+    <div>
+      {error && (
+        <ErrorModal
+          onConfirm={errorHandler}
+          title={error.title}
+          message={error.message}
+        />
+      )}
+      <Card className={styles.input}>
+        <form onSubmit={submitHandler}>
+          <div className="form-inputs-controls">
+            <div className="form-input-control">
+              <input
+                placeholder="Firstname"
+                type="text"
+                value={enteredFirstName}
+                onChange={firstNameChangeHandler}
+              />
+            </div>
+            <div className="form-input-control">
+              <input
+                placeholder="Lastname"
+                type="text"
+                value={enteredLastName}
+                onChange={lastNameChangeHandler}
+              />
+            </div>
+            <div className="form-input-control">
+              <input
+                placeholder="Annual Salary $"
+                type="number"
+                value={enteredAnnualSalary}
+                onChange={annualSalaryChangeHandler}
+              />
+            </div>
+            <div className="form-input-control">
+              <input
+                placeholder="Superannuation %"
+                type="number"
+                value={enteredSuperannuationRate}
+                onChange={superannuationRateChangeHandler}
+              />
+            </div>
+            <div className="form-input-control">
+              <label>Start Period</label>
+              <input
+                placeholder="Start Date"
+                type="Date"
+                value={enteredStartDate}
+                onChange={startDateChangeHandler}
+              />
+            </div>
+            <div className="form-input-control">
+              <label>End Period</label>
+              <input
+                placeholder="End Date"
+                type="Date"
+                value={enteredEndDate}
+                onChange={endDateChangeHandler}
+              />
+            </div>
           </div>
-          <div className="form-input-control">
-            <input
-              placeholder="Lastname"
-              type="text"
-              value={enteredLastName}
-              onChange={lastNameChangeHandler}
-            />
-          </div>
-          <div className="form-input-control">
-            <input
-              placeholder="Annual Salary $"
-              type="number"
-              value={enteredAnnualSalary}
-              onChange={annualSalaryChangeHandler}
-            />
-          </div>
-          <div className="form-input-control">
-            <input
-              placeholder="Superannuation %"
-              type="number"
-              value={enteredSuperannuationRate}
-              onChange={superannuationRateChangeHandler}
-            />
-          </div>
-          <div className="form-input-control">
-            <label>Start Period</label>
-            <input
-              placeholder="Start Date"
-              type="Date"
-              value={enteredStartDate}
-              onChange={startDateChangeHandler}
-            />
-          </div>
-          <div className="form-input-control">
-            <label>End Period</label>
-            <input
-              placeholder="End Date"
-              type="Date"
-              value={enteredEndDate}
-              onChange={endDateChangeHandler}
-            />
-          </div>
-        </div>
-        <Button type="submit">Submit</Button>
-      </form>
-    </Card>
+          <Button type="submit">Submit</Button>
+        </form>
+      </Card>
+    </div>
   );
 };
 
